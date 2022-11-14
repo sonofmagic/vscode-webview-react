b9b6cac845a6bdb02b14a76693dec1d8c63955d7
sonofmagic <1324318532@qq.com>
2022-11-14 23:43:41 +0800

    feat: upgrade react version

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import * as React from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">{count}</h1>
+      </header>
+      <button onClick={() => setCount((x) => x + 1)}>Cli参考me！</button>
+    </div>
+  );
+};
 
 export default App;
